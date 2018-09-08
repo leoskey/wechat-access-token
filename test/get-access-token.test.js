dbb99a663191205ab7c4e18352d2b305a52cedba
leoskey <leichen@live.com>
2018-09-08 13:28:59 +0800

    修复若干问题，添加单元测试。

---
+++ test/get-access-token.test.js
@@ -0,0 +1,58 @@
+const mocha = require('mocha');
+const assert = require('power-assert');
+const { WechatAccessToken, WechatAccessTokenError } = require('../lib/wechat-access-token');
+
+const appConfig = {
+  appID: 'wx5feba7d2251e01e6',
+  appSecret: '4b05a7bf62886a13ba06e13f1414d36e'
+};
+
+describe('wechat-access-token', () => {
+  beforeEach(async() => {
+    const client = new WechatAccessToken('redis://:@127.0.0.1:6379');
+    await client.redis.del(`wechat_access_token/${appConfig.appID}`);
+    this.client = client;
+
+  });
+
+  it('should reuturn latest access token', async() => {
+    const accessTokenResult = await this.client.getAccessToken(appConfig);
+    assert.ok(typeof accessTokenResult.accessToken === 'string');
+
+  });
+
+  it('should reuturn cache access token', async() => {
+    const accessTokenFirstResult = this.client.getAccessToken(appConfig);
+    const accessTokenSecondResult = this.client.getAccessToken(appConfig);
+    const accessTokenThirdResult = this.client.getAccessToken(appConfig);
+    const values = await Promise.all([accessTokenFirstResult, accessTokenSecondResult, accessTokenThirdResult]);
+    assert.equal(values[0].accessToken, values[1].accessToken);
+    assert.equal(values[0].accessToken, values[2].accessToken);
+
+  });
+
+  it('should return wechat config error', async() => {
+    await (async() => {
+      try {
+        await this.client.getAccessToken(null);
+      } catch (error) {
+        assert.throws(error, /WechatAccessTokenError/);
+      }
+    });
+
+  });
+
+  it('should return wechat api error', async() => {
+    await (async() => {
+      try {
+        await this.client.getAccessToken({ appID: 'wx', appSecret: 'wx' });
+      } catch (error) {
+        console.log('执行')
+        console.log(error)
+        assert.throws(error, /WechatAccessTokenError/);
+      }
+    });
+
+  });
+
+});
